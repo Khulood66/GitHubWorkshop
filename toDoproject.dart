46458ef59d43c4ea96a7ccfd670798c10a4f5ba2
Khulood Khaled <khuloodkhaled97@gmail.com>
2024-07-13 02:16:38 +0300

    Complete Enhancement DisplayTasks function

--- toDoproject.dart
+++ toDoproject.dart
@@ -1,7 +1,14 @@
 //function Remove TAsk from RemoveTask branch 
-
+//Enhanching display tasks by displayed with date
 void displayTasks(List<Map<String, String>> taskList) {
   for (var i = 0; i < taskList.length; i++) {
-    print('${i}: ${taskList[i]['task']}');
+    print('${i}: ${taskList[i]['task']} (Due: ${taskList[i]['dueDate']})');
   }
 }
+
+// void displayTasksWithDueDates(List<Map<String, String>> taskList) {
+//   for (var i = 0; i < taskList.length; i++) {
+//     print('${i}: ${taskList[i]['task']} (Due: ${taskList[i]['dueDate']})');
+//   }
+// }
+
